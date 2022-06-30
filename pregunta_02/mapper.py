87fbff72495b509d9171d33a88c189544233af9d
Jaillier1 <95106251+Jaillier1@users.noreply.github.com>
2022-06-30 15:32:18 -0500

    Update mapper.py

--- pregunta_02/mapper.py
+++ pregunta_02/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+! /usr/bin/ python3
+
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        lista = line.split(',')
+        sys.stdout.write("{}\t{}\n".format(lista[3],lista[4]))
