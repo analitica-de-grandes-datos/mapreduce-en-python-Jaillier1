0dbd258044c6089d8be86e59cc3c8831c18ea1c2
Jaillier1 <95106251+Jaillier1@users.noreply.github.com>
2022-06-30 15:35:48 -0500

    Update reducer.py

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,16 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+import itertools 
+
+if __name__ == '__main__':
+    curkey=None
+    total=0
+
+    for line in sys.stdin:
+        key, val = line.split("\n")
+        val = int(val)
+
+
+    sys.stdout.write("{}\t{}\n".format(key,sorted( value)))
